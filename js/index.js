9bb9d970493db6929f641171519f6e42a4552767
Remania <remaniacode@gmail.com>
2022-06-04 18:56:05 -0500

    colorizer v1.1.2: fixing toggle-theme-button text

--- js/index.js
+++ js/index.js
@@ -12,13 +12,14 @@ const app__sidebar = document.querySelector(".App__sidebar");
 const color__form = document.getElementById("color__form");
 const color = document.getElementById("color");
 const colors__list = document.getElementById("colors__list");
-const delete_all_colors_btn = document.getElementById("delete_all_colors_btn");
+/* const delete_all_colors_btn = document.getElementById("delete_all_colors_btn"); */
 const menu_btn = document.getElementById("menu-btn");
 const toggle_theme_btn = document.getElementById("toggle-theme-btn");
 const user = new User();
 
 const loadThemeColor = (theme) => {
   user.setCurrentTheme(theme);
+  toggle_theme_btn.textContent = user.getCurrentTheme()
   app.setAttribute("data-theme", user.getCurrentTheme());
   toggle_theme_btn.textContent = `${
     theme.charAt(0).toUpperCase() + theme.slice(1)
@@ -78,14 +79,14 @@ menu_btn.addEventListener("click", function () {
 const toggleDarkTheme = () => {
   user.setCurrentTheme("dark");
   app.setAttribute("data-theme", user.getCurrentTheme());
-  toggle_theme_btn.textContent = "Light Theme";
+  toggle_theme_btn.textContent = "Dark Theme";
   localStorage.setItem("colorizer-theme", "dark");
 };
 
 const toggleLightTheme = () => {
   user.setCurrentTheme("light");
   app.setAttribute("data-theme", user.getCurrentTheme());
-  toggle_theme_btn.textContent = "Dark Theme";
+  toggle_theme_btn.textContent = "Light Theme";
   localStorage.setItem("colorizer-theme", "light");
 };
 
